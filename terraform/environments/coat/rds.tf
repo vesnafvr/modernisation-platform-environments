c8a86a7cd1ecf30546448660f80cc8c769a165bc
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-04-24 13:24:27 +0100

    update aurora sql version

--- terraform/environments/coat/rds.tf
+++ terraform/environments/coat/rds.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "test_db_sg" {
 resource "aws_rds_cluster" "test_db_cluster" {
   cluster_identifier      = "test-db-cluster"
   engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
+  engine_version          = "8.0.mysql_aurora.3.07.1"
   availability_zones      = ["eu-west-2a", "eu-west-2b"]
   database_name           = "mydb"
   master_username         = "foo"
